366ba2169ec124aa9afe88380bc286d6e105ac10
Sajjal <sajjalco@gmail.com>
2020-07-19 12:45:32 -0500

    Auto Eff-CertBot-LetsEncrypt SSL Configuration

--- modules/generateConf.js
+++ modules/generateConf.js
@@ -31,9 +31,9 @@ async function generateConfSSL(data, serverName, sudoPass) {
   ServerName ${serverName}${"\n"}
   <Proxy balancer://mycluster>`;
 
-  let partB = "\n";
+  let partB = `${"\n"}`;
   for (let i = 0; i < data.length; i++) {
-    partB = partB + `${"\t"}${"\t"}BalancerMember http://127.0.0.1:${data[i].port}` + "\n";
+    partB = partB + `${"\t"}${"\t"}BalancerMember http://127.0.0.1:${data[i].port}${"\n"}`;
   }
 
   const partC = `${"\t"}</Proxy>${"\n"}
@@ -47,9 +47,26 @@ async function generateConfSSL(data, serverName, sudoPass) {
 
   </VirtualHost>`;
 
+  const partAa = `<IfModule mod_ssl.c>
+  <VirtualHost *:443>${"\n"}
+    ServerName ${serverName}${"\n"}
+    <Proxy balancer://mycluster>`;
+
+  const partD = `${"\t"}</Proxy>${"\n"}
+  ProxyPreserveHost On
+  ProxyPass / balancer://mycluster/
+  ProxyPassReverse / balancer://mycluster/${"\n"}
+  Include /etc/letsencrypt/options-ssl-apache.conf
+  SSLCertificateFile /etc/letsencrypt/live/${serverName}/fullchain.pem
+  SSLCertificateKeyFile /etc/letsencrypt/live/${serverName}/privkey.pem${"\n"}
+  </VirtualHost>
+</IfModule>`;
+
   const info = partA + partB + partC;
+  const infoSSL = partAa + partB + partD;
 
   await createFile(`/etc/apache2/sites-available/${serverName}.conf`, info);
+  await createFile(`/etc/apache2/sites-available/${serverName}-le-ssl.conf`, infoSSL);
   await linuxCommand(`echo "${sudoPass}" | sudo -S a2ensite ${serverName}.conf`);
   await linuxCommand(`echo "${sudoPass}" | sudo -S service apache2 reload`);
   await linuxCommand(`echo "${sudoPass}" | sudo -S systemctl restart apache2`);
